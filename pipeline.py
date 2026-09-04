58c1edc7de34395d40105fa32f6c88f5a2b101e6
Rama <41026094+raghavrama21@users.noreply.github.com>
2026-09-04 00:18:32 -0400

    ll_tuner_job

--- pipeline.py
+++ pipeline.py
@@ -23,6 +23,8 @@ REGION = boto3.Session().region_name
 pipeline_session = PipelineSession()
 
 XGB_IMAGE = retrieve("xgboost", REGION, version="1.7-1")
+LL_IMAGE = retrieve("linear-learner", REGION, version="1.7-1")
+
 
 sklearn_processor = SKLearnProcessor(
     framework_version="1.2-1",
@@ -116,10 +118,61 @@ xgb_tuning_step = TuningStep(
     step_args=step_args,
 )
 
+ll_estimator = Estimator(
+    image_uri=LL_IMAGE,
+    role=ROLE,
+    instance_type="ml.m5.xlarge",
+    instance_count=1,
+    output_path=f"s3://{BUCKET}/models/ll/",
+    sagemaker_session=pipeline_session,
+)
+
+ll_estimator.set_hyperparameters(
+    predictor_type="binary_classifier",
+)
+
+ll_tuner = HyperparameterTuner(
+    estimator=ll_estimator,
+    objective_metric_name="validation:roc_auc_score",
+    objective_type="Maximize",
+    hyperparameter_ranges={
+        "wd": ContinuousParameter(0, 1),
+        "l1": ContinuousParameter(0, 1),
+        "learning_rate": ContinuousParameter(0, 1),
+        "mini_batch_size": IntegerParameter(100, 5000),
+        "use_bias": CategoricalParameter([True, False]),
+    },
+    max_jobs=4,
+    max_parallel_jobs=2,
+)
+
+with Run(
+    experiment_name=EXPERIMENT_NAME,
+    run_name="LL-run",  # remember: no underscores
+    sagemaker_session=pipeline_session,
+) as run:
+    ll_step_args = ll_tuner.fit(
+        inputs={
+            "train": TrainingInput(
+                s3_data=preprocess_step.properties.ProcessingOutputConfig.Outputs[
+                    "train"
+                ].S3Output.S3Uri,
+                content_type="text/csv",
+            ),
+            "validation": TrainingInput(
+                s3_data=preprocess_step.properties.ProcessingOutputConfig.Outputs[
+                    "validation"
+                ].S3Output.S3Uri,
+                content_type="text/csv",
+            ),
+        }
+    )
+
+ll_tuning_step = TuningStep(name="TuneLinearLearner", step_args=ll_step_args)
 
 pipeline = Pipeline(
     name=PIPELINE_NAME,
-    steps=[preprocess_step, xgb_tuning_step],
+    steps=[preprocess_step, xgb_tuning_step, ll_tuning_step],
 )
 if __name__ == "__main__":
     pipeline.upsert(role_arn=ROLE)
